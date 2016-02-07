239c73f432828b1f20f3c82b0d67abadc4c0ce5d
chloerevery <chloerevery@gmail.com>
2016-02-06 19:30:51 -0500

    Update dine-today.js

--- dine-today.js
+++ dine-today.js
@@ -3,6 +3,7 @@ Customers = new Mongo.Collection("customers");
 
 if (Meteor.isClient) {
   Session.setDefault('counter', 0);
+
   Template.body.helpers({
       //  deals: [
       //   { text: "This is deal 1" },
@@ -10,6 +11,7 @@ if (Meteor.isClient) {
       //   { text: "This is deal 3" }
       // ]
       customers: function () {
+
         return Customers.find({});
       }
 
@@ -27,10 +29,98 @@ if (Meteor.isClient) {
 
   });
 
+  Template.settings.events({
+    "click .settingsName": function(event) {
+      $(".settingsName").toggleClass("hidden");
+      $(".changeName").toggleClass("hidden");
+    },
+
+    "click .settingsPhone": function(event) {
+      $(".settingsPhone").toggleClass("hidden");
+      $(".changePhone").toggleClass("hidden");
+    },
+
+    "click .settingsEmail": function(event) {
+      $(".settingsEmail").toggleClass("hidden");
+      $(".changeEmail").toggleClass("hidden");
+    },
+
+    "click .settingsAllergies": function(event) {
+      $(".settingsAllergies").toggleClass("hidden");
+      $(".changeAllergies").toggleClass("hidden");
+    },
+
+    "click .settingsZip": function(event) {
+      $(".settingsZip").toggleClass("hidden");
+      $(".changeZip").toggleClass("hidden");
+    },
+
+    //change setting button clicks
+    "submit .change-name": function(event) {
+      event.preventDefault();
+      console.log("Submit button clicked");
+      var name = event.target.name.value;
+      var id = this._id;
+      Customers.update({
+        _id: id}, {$set: {name: name}});
+      $(".settingsName").toggleClass("hidden");
+      $(".changeName").toggleClass("hidden");
+    },
+
+    "submit .change-phone": function() {
+      event.preventDefault();
+      console.log("Submit button clicked");
+      var phone = event.target.phone.value;
+      var id = this._id;
+      console.log("other id: " + id)
+      Customers.update({
+        _id: id}, {$set: {phone: phone}});
+      $(".settingsPhone").toggleClass("hidden");
+      $(".changePhone").toggleClass("hidden");
+    },
+
+    "submit .change-email": function() {
+      event.preventDefault();
+      console.log("Submit button clicked");
+      var email = event.target.email.value;
+      console.log("email is: " + email);
+      var id = this._id;
+      console.log("id for email is: " + id);
+      Customers.update({
+        _id: id}, {$set: {email: email}});
+      $(".settingsEmail").toggleClass("hidden");
+      $(".changeEmail").toggleClass("hidden");
+    },
+
+    "submit .change-allergies": function() {
+      event.preventDefault();
+      console.log("Submit button clicked");
+      var allergies = event.target.allergies.value;
+      var id = this._id;
+      Customers.update({
+        _id: id}, {$set: {allergies: allergies}});
+      $(".settingsAllergies").toggleClass("hidden");
+      $(".changeAllergies").toggleClass("hidden");
+    },
+
+    "submit .change-zip": function() {
+      event.preventDefault();
+      console.log("Submit button clicked");
+      var zip = event.target.zip.value;
+      var id = this._id;
+      Customers.update({
+        _id: id}, {$set: {zip: zip}});
+      $(".settingsZip").toggleClass("hidden");
+      $(".changeZip").toggleClass("hidden");
+      }
+
+  });
+
   Template.signUp.events({
-    "submit .new-user": function(event) {
+    "submit .new-user": function() {
       console.log("Submit button clicked");
       event.preventDefault();
+      var restaurant = event.target.name.value;
 
       var name = event.target.name.value;
       var phone = event.target.phone.value;
@@ -38,8 +128,8 @@ if (Meteor.isClient) {
       var allergies = event.target.allergies.value;
       var zip = event.target.zip.value;
       var userid = Meteor.userId();
-
       Customers.insert({
+        restaurant: restaurant,
         userid: userid,
         name: name,
         phone: phone,
@@ -69,12 +159,6 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.si.events({
-    'click .signIn': function() {
-       $("#signIn").toggleClass("hidden");
-     }
-
-  });
 
   Template.rest.events({
     'click .restaurants': function() {
